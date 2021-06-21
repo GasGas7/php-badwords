6697bd9d15cd9cef61126569d135a08e4b05d3f2
GasGas7 <79295810+GasGas7@users.noreply.github.com>
2021-06-21 15:00:28 +0200

    new $var + var_dump;

---
+++ index.php
@@ -0,0 +1,16 @@
+<!-- 
+    - Creare una variabile con un paragrafo di testo a vostra scelta. 
+    - Stampare a schermo il paragrafo e la sua lunghezza.
+    - Una parola da censurare viene passata dall'utente tramite parametro GET.
+    - Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+ -->
+<?php 
+$nome = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+Architecto excepturi laboriosam adipisci incidunt, maxime quas doloremque ipsum dicta ducimus possimus!';
+
+var_dump($nome);
+
+?>
+
+
+ 
